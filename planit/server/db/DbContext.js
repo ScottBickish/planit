66e475b87d09588b406444a8648515a18984052e
Scott Bickish <sbickish3@gmail.com>
2021-11-15 12:27:54 -0700

    init

--- planit/server/db/DbContext.js
+++ planit/server/db/DbContext.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { ProjectSchema } from '../models/Project'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
+  Projects = mongoose.model('Project', ProjectSchema)
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
