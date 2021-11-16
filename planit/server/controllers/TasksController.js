f474bb3e76708af99e423f75dd7724a40c1d90a0
Scott Bickish <sbickish3@gmail.com>
2021-11-16 11:05:36 -0700

    tasks complete

--- planit/server/controllers/TasksController.js
+++ planit/server/controllers/TasksController.js
@@ -4,17 +4,54 @@ import BaseController from '../utils/BaseController'
 
 export class TasksController extends BaseController {
   constructor() {
-    super('api/projects/:projectId/tasks')
+    super('api/projects')
     this.router
-      .get('', this.getTasks)
+      .get('/:projectId/tasks', this.getTasks)
       .use(Auth0Provider.getAuthorizedUserInfo)
-      // .post('', this.create)
+      .post('/:projectId/tasks', this.create)
+      .put('/:projectId/tasks/:taskId', this.edit)
+      .delete('/:projectId/tasks/:taskId', this.removeTask)
   }
 
   async getTasks(req, res, next) {
     try {
       const tasks = await tasksService.getTasks(req.params.projectId)
       return res.send(tasks)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      req.body.projectId = req.params.projectId
+
+      const task = await tasksService.create(req.body)
+      return res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      req.body.projectId = req.params.projectId
+      req.body.id = req.params.taskId
+      const task = await tasksService.edit(req.body)
+      return res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async removeTask(req, res, next) {
+    try {
+      const userId = req.userInfo.id
+      const taskId = req.params.taskId
+      res.send('you have deleted this task')
+      await tasksService.removeTask(taskId, userId)
     } catch (error) {
       next(error)
     }
--- planit/server/services/TasksService.js
+++ planit/server/services/TasksService.js
@@ -9,6 +9,36 @@ class TasksService {
     }
     return foundTask
   }
+
+  async getTaskById(id) {
+    const foundTask = await dbContext.Task.findById(id).populate('creator')
+    if (!foundTask) {
+      throw new BadRequest('null checking issue')
+    }
+    return foundTask
+  }
+
+  async create(body) {
+    const newTask = await dbContext.Task.create(body)
+    return newTask.populate('creator')
+  }
+
+  async edit(body) {
+    const task = await this.getTaskById(body.id)
+    if (task.creatorId.toString() !== body.creatorId) {
+      throw new Forbidden('you shall not pass')
+    }
+    const updatedTask = dbContext.Task.findOneAndUpdate({ _id: body.id, creatorId: body.creatorId }, body, { new: true })
+    return updatedTask
+  }
+
+  async removeTask(taskId, userId) {
+    const task = await this.getTaskById(taskId)
+    if (task.creatorId.toString() !== userId) {
+      throw new Forbidden('you cannot delete this task')
+    }
+    await dbContext.Task.findByIdAndDelete(taskId)
+  }
 }
 
 export const tasksService = new TasksService()
