8ba5aec51064819c57ff5a42b6379f12d69fe445
Greg Bullington <gbullington23@gmail.com>
2021-11-17 15:59:21 -0700

    project-page-layout

--- planit.client/src/pages/ProjectPage.vue
+++ planit.client/src/pages/ProjectPage.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="row container-fluid" v-if="account.id">
-    <div class="col-1 mt-3" id="edit-div">
+  <div class="row container-fluid ps-0">
+    <div class="col-2 mt-3 ms-0 p-0" id="edit-div">
       <button
         class="btn gradient text-light px-5"
         data-bs-toggle="offcanvas"
         href="#projects-offcanvas"
-        style="font-size: 50px"
+        style="font-size: 45px"
       >
         P
       </button>
     </div>
-    <div class="col text-center">
-      <h1>{{ activeProject.name }}</h1>
-      <h6>{{ activeProject.description }}</h6>
+    <div class="col-md-10 my-4">
+      <div class="row align-items-center">
+        <h1 style="font-size: 60px;" >{{ activeProject.name }}</h1>
+      </div>
+      <div class="row">
+        <p>{{ activeProject.description }}</p>
+      </div>
+      <div class="row text-align-center">
+        <div class="col-8 my-5">
+          <h3 class="gradient-text">Sprints</h3>
+          <p>Group your tasks into sprints for over-arching collections for better organization.</p>
+        </div>
+        <div class="col-4 text-end">
+          <button
+            style="font-size: 18px;"
+            class="btn btn-lg rounded btn-outline-custom gradient-text px-5"
+            data-bs-toggle="modal"
+            data-bs-target="#SprintForm"
+            title="create-sprint"
+          >
+            Add Sprint
+          </button>
+        </div>
+      </div>
+      <div class="row">
+
+      </div>
     </div>
     <!-- NOTE this is just the off canvas on the page  -->
     <ProjectsComponent />
