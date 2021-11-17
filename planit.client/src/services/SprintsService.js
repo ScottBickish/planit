09ea8b2742280cb0418139451e26f59954d90f64
Scott Bickish <sbickish3@gmail.com>
2021-11-17 16:45:50 -0700

    functions made

--- planit.client/src/services/SprintsService.js
+++ planit.client/src/services/SprintsService.js
@@ -1,6 +1,21 @@
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
 
 class SprintsService {
-
+async createSprint(id){
+  const res = await api.post(`api/projects/${id}/sprints`)
+  logger.log(res.data)
+  AppState.sprints = [...AppState.sprints, res.data]
+}
+async getSprints(id){
+  const res = await api.get(`api/projects/${id}/sprints`)
+  AppState.sprints = res.data
+}
+async removeSprint(pId, id){
+  await api.delete(`api/projects/${pId}/sprints/${id}`)
+  AppState.sprints = AppState.sprints.filter(s => s.id !== AppState.sprints.id)
+}
 
 }
 
