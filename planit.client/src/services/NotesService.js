bdaad0f2c705479e12b0e6d5f05ceee043532dd5
Scott Bickish <sbickish3@gmail.com>
2021-11-18 15:23:53 -0700

    service notes done

--- planit.client/src/services/NotesService.js
+++ planit.client/src/services/NotesService.js
@@ -1,6 +1,25 @@
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
 
-
+// NOTE NEED ROUTE FOR CREATE IN NOTESCOMPONENT and the whole note to send
 class NotesService {
+  async createNote(pId, note){
+    const res = await api.post(`api/projects/${pId}/notes`, note)
+    logger.log(res.data)
+    AppState.notes = [...AppState.notes, res.data]
+  }
+  // NOTE need a task id on the body like the task for the sprints
+  async getNotesForProject(pId){
+    const res = await api.get(`api/projects/${pId}/notes`)
+    AppState.notes = res.data
+  }
+  async removeNote(pId, nId){
+    await api.delete(`api/projects/${pId}/notes/${nId}`)
+    AppState.notes = AppState.notes.filter(n => n.id !== AppState.notes.id)
+    this.getNotesForProject(pId)
+    
+  }
 
 
 }
